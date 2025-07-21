7f87b98312260964f7ae8cb1a1743ea7364ff43e
gram <git@orsinium.dev>
2025-07-21 19:23:04 +0200

    render cracks on bricks

--- game/brick.go
+++ game/brick.go
@@ -5,8 +5,7 @@ import (
 )
 
 var (
-	brickSize  = firefly.Size{W: 16, H: 16}
-	brickStyle = firefly.Style{FillColor: firefly.ColorOrange}
+	brickSize = firefly.Size{W: 16, H: 16}
 )
 
 type Brick struct {
@@ -17,7 +16,7 @@ type Brick struct {
 func newBrick(x, y int) *Brick {
 	return &Brick{
 		pos:    firefly.Point{X: x, Y: y},
-		health: 3,
+		health: 4,
 	}
 }
 
@@ -38,5 +37,45 @@ func (b Brick) bottom() int {
 }
 
 func (b Brick) render() {
-	firefly.DrawRect(b.pos, brickSize, brickStyle)
+	{
+		style := firefly.Style{FillColor: firefly.ColorOrange}
+		firefly.DrawRect(b.pos, brickSize, style)
+	}
+	{
+		style := firefly.Style{StrokeColor: firefly.ColorWhite, StrokeWidth: 1}
+		firefly.DrawRect(
+			b.pos.Add(firefly.Point{X: 2, Y: 2}),
+			brickSize.Sub(firefly.Size{W: 4, H: 4}),
+			style,
+		)
+	}
+	b.renderCracks()
+}
+
+func (b Brick) renderCracks() {
+	style := firefly.Style{FillColor: firefly.ColorWhite}
+	if b.health <= 3 {
+		firefly.DrawTriangle(
+			firefly.Point{X: b.pos.X, Y: b.pos.Y + 2},
+			firefly.Point{X: b.pos.X, Y: b.pos.Y + 4},
+			firefly.Point{X: b.pos.X + 2, Y: b.pos.Y + 3},
+			style,
+		)
+	}
+	if b.health <= 2 {
+		firefly.DrawTriangle(
+			firefly.Point{X: b.pos.X + brickSize.W, Y: b.pos.Y + 4},
+			firefly.Point{X: b.pos.X + brickSize.W, Y: b.pos.Y + 6},
+			firefly.Point{X: b.pos.X + brickSize.W - 2, Y: b.pos.Y + 5},
+			style,
+		)
+	}
+	if b.health <= 1 {
+		firefly.DrawTriangle(
+			firefly.Point{X: b.pos.X + 2, Y: b.pos.Y + brickSize.H},
+			firefly.Point{X: b.pos.X + 6, Y: b.pos.Y + brickSize.H},
+			firefly.Point{X: b.pos.X + 4, Y: b.pos.Y + brickSize.H - 4},
+			style,
+		)
+	}
 }
