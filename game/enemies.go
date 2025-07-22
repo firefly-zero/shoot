15cf44f25dccc6afdb015309e5ea779eb2b25d33
gram <git@orsinium.dev>
2025-07-22 17:53:40 +0200

    randomize enemy spawn

--- game/enemies.go
+++ game/enemies.go
@@ -14,10 +14,21 @@ func (es *Enemies) update() {
 	}
 	if es.nextWave == 0 {
 		es.nextWave = 60
+		var pos firefly.Point
+		switch firefly.GetRandom() % 4 {
+		case 0:
+			pos = firefly.Point{X: -10, Y: -10}
+		case 1:
+			pos = firefly.Point{X: firefly.Width + 10, Y: -10}
+		case 2:
+			pos = firefly.Point{X: 10, Y: firefly.Height + 10}
+		default:
+			pos = firefly.Point{X: firefly.Width + 10, Y: firefly.Height + 10}
+		}
 		if es.items.len() < 5 {
 			es.items.add(&Enemy{
 				id:     es.nextID,
-				pos:    firefly.Point{X: -10, Y: -10},
+				pos:    pos,
 				d:      8,
 				health: 1,
 			})
